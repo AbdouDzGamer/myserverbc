fabf9a010628ca89d773804b47d934d944753a10
AbdouDzGamer <50796461+AbdouDzGamer@users.noreply.github.com>
2019-08-05 22:11:20 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    TOKEN: 'NjA3NTg0NzY0NDM3OTg3MzY4.XUbvdA.L-Nly4FNY2rOpV3hMCw6DRUj20w',    
-    YT_API_KEY: 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8',
-    prefix: '$',
-    devs: ['380827398767378433']
-}//LUXY
